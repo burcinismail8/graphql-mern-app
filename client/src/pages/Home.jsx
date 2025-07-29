1a3f953dcba2be6ed6ea6c07a84ead4402fe0bdd
burcinismail8 <burcinismail8@gmail.com>
2025-07-29 12:27:01 +0300

    Get Clients For Select. Add Project Mutation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import AddClientModel from "../components/AddClientModel";
 import Projects from "../components/Projects";
 import Clients from "../components/Clients";
+import AddProjectModel from "../components/AddProjectModel";
 
 const Home = () => {
   return (
     <>
       <div className="d-flex gap-3 mb-4">
         <AddClientModel />
+        <AddProjectModel />
       </div>
       <Projects />
       <hr />
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -3,6 +3,7 @@ import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
 import { Link, useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
+import ClientInfo from "../components/ClientInfo";
 
 const Project = () => {
   const { id } = useParams();
@@ -20,6 +21,7 @@ const Project = () => {
           <p>{data.project.description}</p>
           <h5 className="mt-3">Project Status</h5>
           <p className="lead">{data.project.status}</p>
+          <ClientInfo client={data.project.client} />
         </div>
       )}
     </>
